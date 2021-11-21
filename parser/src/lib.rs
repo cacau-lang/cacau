2e2ebaaf43bae98246a5cf6d84d0529c4886f37a
Vinícius Rodrigues Miguel <lemao.vrm07@hotmail.com>
2021-11-21 00:30:05 -0300

    First commit: adds a WIP parser

---
+++ parser/src/lib.rs
@@ -0,0 +1,109 @@
+use pest_derive::Parser;
+
+#[derive(Parser)]
+#[grammar = "grammar.pest"]
+pub struct ExpressionParser;
+
+#[cfg(test)]
+mod tests {
+    use pest::Parser;
+
+    use crate::Rule;
+    use crate::ExpressionParser;
+
+    fn parse(rule: Rule, expression: &str) -> Option<()> {
+        ExpressionParser::parse(rule, expression).ok().map(|_| ())
+    }
+
+    fn assert_parses(rule: Rule, expression: &str) {
+        assert!(parse(rule, expression).is_some())
+    }
+
+    fn assert_does_not_parse(rule: Rule, expression: &str) {
+        assert!(parse(rule, expression).is_none())
+    }
+
+    #[test]
+    fn single_char_identifiers() {
+        assert_parses(Rule::identifier, "x");
+        assert_parses(Rule::identifier, "z");
+
+        assert_does_not_parse(Rule::identifier, "2");
+        assert_does_not_parse(Rule::identifier, "7");   
+    }
+
+    #[test]
+    fn valid_identifiers() {
+        assert_parses(Rule::identifier, "a");
+        assert_parses(Rule::identifier, "abc");
+        assert_parses(Rule::identifier, "abc123");
+        assert_parses(Rule::identifier, "zazz123");
+    }
+
+    // TODO: this test is failing ;-;
+    // #[test]
+    // pub fn invalid_identifiers() {
+    //     assert_does_not_parse(Rule::identifier, "2");
+    //     assert_does_not_parse(Rule::identifier, "3abc");
+    //     assert_does_not_parse(Rule::identifier, "@abc123");
+    //     assert_does_not_parse(Rule::identifier, "zaz@z123");
+    // }
+
+    #[test]
+    fn integers() {
+        assert_parses(Rule::integer, "1234");
+        assert_parses(Rule::integer, "000000");
+        assert_parses(Rule::integer, "987654321");
+        assert_parses(Rule::integer, "987654321");
+    }
+
+
+    #[test]
+    fn floats() {
+        assert_parses(Rule::float, "123.4");
+        assert_parses(Rule::float, "000.000");
+        assert_parses(Rule::float, "98.7654321");
+        assert_parses(Rule::float, "987654.321");
+
+        assert_does_not_parse(Rule::float, "123");
+        assert_does_not_parse(Rule::float, "50000");
+        assert_does_not_parse(Rule::float, "abc123");
+    }
+
+    #[test]
+    fn type_annotations() {
+        assert_parses(Rule::type_annotation, ": int");
+        assert_parses(Rule::type_annotation, ": float");
+        assert_parses(Rule::type_annotation, ": string");
+
+        assert_does_not_parse(Rule::type_annotation, "123");
+        assert_does_not_parse(Rule::type_annotation, "string");
+        assert_does_not_parse(Rule::type_annotation, ": 123");
+    }
+
+    #[test]
+    fn assignment() {
+        // TODO: add other kinds of expressions here when possible
+
+        assert_parses(Rule::assignment, "let x = 123");
+        assert_parses(Rule::assignment, "let nines = 999");
+
+        assert_parses(Rule::assignment, "let six: int = 5");
+
+        assert_parses(Rule::assignment, "let six: BigDecimal = 10");
+        
+        assert_does_not_parse(Rule::assignment, "let name");
+        assert_does_not_parse(Rule::assignment, "let ch x: b");
+        assert_does_not_parse(Rule::assignment, "six: BigDecimal = 10");
+    }
+
+    #[test]
+    fn function_argument() {
+        assert_parses(Rule::function_argument, "x: int");
+        assert_parses(Rule::function_argument, "y: string");
+        assert_parses(Rule::function_argument, "lambda: num");
+        
+        assert_does_not_parse(Rule::function_argument, "x: 123");
+        assert_does_not_parse(Rule::function_argument, "y: \"Hey\"");
+    }
+}
