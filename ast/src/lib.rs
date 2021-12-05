2d22ae5bf32c2530851d5aeb78c6c03b3e310b8f
Bráulio Bezerra <brauliobezerra@gmail.com>
2021-12-05 12:01:01 -0300

    parser: lalrpop experiments

---
+++ ast/src/lib.rs
@@ -0,0 +1,130 @@
+#[derive(Debug, PartialEq)]
+pub struct CacauProgram {
+    pub items: Vec<Statement>,
+}
+
+#[derive(Debug, PartialEq)]
+pub enum Statement {
+    Fn(Function),
+    Struct(),
+    Enum(),
+    Expression(Expr),
+}
+
+#[derive(Debug, PartialEq)]
+pub struct Function {
+    pub public: bool,
+    pub name: String,
+    pub params: Vec<FunctionArgument>,
+    pub output: String,
+    pub body: Expr,
+}
+
+#[derive(Debug, PartialEq)]
+pub struct Struct {
+    pub name: String,
+    // TODO fields and their types
+}
+
+#[derive(Debug, PartialEq)]
+pub struct Enum {
+    pub name: String,
+    // TODO variants
+}
+
+#[derive(Debug, PartialEq)]
+pub struct Assignment {
+    pub name: String,
+    pub type_annotation: Option<String>,
+    pub expression: Expr,
+}
+
+#[derive(Debug, PartialEq)]
+pub enum Expr {
+    Lit(Lit),
+    Id(String),
+    Assign(Box<Assignment>),
+    FnCall(Box<FnCall>),
+    Arith(Box<ArithExpr>),
+    Cmp(Box<CmpExpr>),
+    Logic(Box<LogicExpr>),
+    Paren(Box<Expr>),
+    Unary(Box<Expr>),
+}
+
+#[derive(Debug, PartialEq)]
+pub enum Lit {
+    Bool(bool),
+    Int(i64),
+    Float(f64),
+    Char(char),
+    String(String),
+}
+
+#[derive(Debug, PartialEq)]
+pub enum Unary {
+    Not(Box<Expr>),
+    Minus(Box<Expr>),
+}
+
+#[derive(Debug, PartialEq)]
+pub struct Term(pub Expr);
+
+#[derive(Debug, PartialEq)]
+pub struct ArithExpr {
+    pub left: Expr,
+    pub op: ArithOp,
+    pub right: Expr,
+}
+
+#[derive(Debug, PartialEq)]
+pub struct CmpExpr {
+    pub left: Expr,
+    pub op: CmpOp,
+    pub right: Expr,
+}
+
+#[derive(Debug, PartialEq)]
+pub struct LogicExpr {
+    pub left: Expr,
+    pub op: LogicOp,
+    pub right: Expr,
+}
+
+#[derive(Debug, PartialEq)]
+pub enum ArithOp {
+    Add,
+    Sub,
+    Mul,
+    Div,
+    Pow,
+    Mod,
+}
+
+#[derive(Debug, PartialEq)]
+pub enum LogicOp {
+    Or,
+    And,
+}
+
+#[derive(Debug, PartialEq)]
+pub enum CmpOp {
+    EQ,
+    NE,
+    LT,
+    LE,
+    GT,
+    GE,
+}
+
+#[derive(Debug, PartialEq)]
+pub struct FnCall {
+    pub callee: Expr,
+    pub params: Vec<Expr>,
+}
+
+#[derive(Debug, PartialEq)]
+pub struct FunctionArgument {
+    pub name: String,
+    pub type_: String,
+}
---
+++ parser_lalrpop/src/lib.rs
@@ -0,0 +1,151 @@
+#[macro_use]
+extern crate lalrpop_util;
+
+lalrpop_mod!(pub cacau);
+
+#[cfg(test)]
+mod test {
+
+    use ast::ArithOp::*;
+    use ast::Lit::*;
+    use ast::*;
+
+    fn b<T>(t: T) -> Box<T> {
+        Box::new(t)
+    }
+    fn parse_exp(s: &str) -> Expr {
+        crate::cacau::ExprParser::new().parse(s).unwrap()
+    }
+    fn parse_program(s: &str) -> CacauProgram {
+        crate::cacau::CacauProgramParser::new().parse(s).unwrap()
+    }
+    fn l(n: i64) -> Expr {
+        Expr::Lit(Int(n))
+    }
+    fn a(left: Expr, op: ArithOp, right: Expr) -> Expr {
+        Expr::Arith(b(ArithExpr { left, op, right }))
+    }
+
+    #[test]
+    fn test() {
+        assert_eq!(parse_exp("22"), l(22));
+        assert_eq!(parse_exp("(22)"), Expr::Paren(b(l(22))));
+        assert_eq!(parse_exp("((22))"), Expr::Paren(b(Expr::Paren(b(l(22))))));
+
+        assert_eq!(parse_exp("22 + 21"), a(l(22), Add, l(21)));
+        assert_eq!(
+            parse_exp("22 + 21 + 20"),
+            a(a(l(22), Add, l(21)), Add, l(20))
+        );
+
+        #[rustfmt::skip]
+        assert_eq!(
+            parse_exp("22 + 21 * (10 - 5) - 10 * 8 / 9"),
+            a(
+                a(
+                    l(22),
+                    Add,
+                    a(
+                        l(21),
+                        Mul,
+                        Expr::Paren(b(
+                            a(
+                                l(10),
+                                Sub,
+                                l(5)
+                            )
+                        )),
+                    ),
+                ),
+                Sub,
+                a(
+                    a(
+                        l(10),
+                        Mul,
+                        l(8)
+                    ),
+                    Div,
+                    l(9),
+                )
+            )
+        );
+    }
+
+    #[test]
+    fn empty_program() {
+        assert_eq!(parse_program(""), CacauProgram { items: vec![] });
+    }
+
+    #[test]
+    fn hello_world() {
+        assert_eq!(
+            parse_program(r#"println("Hello, world!");"#),
+            CacauProgram {
+                items: vec![Statement::Expression(Expr::FnCall(b(FnCall {
+                    callee: Expr::Id("println".into()),
+                    params: vec![Expr::Lit(Lit::String("Hello, world!".into()))]
+                })))]
+            }
+        );
+    }
+
+    #[test]
+    fn empty_string() {
+        assert_eq!(parse_exp(r#""""#), Expr::Lit(Lit::String("".into())))
+    }
+
+    #[test]
+    fn nonempty_string() {
+        assert_eq!(
+            parse_exp(r#""Test String""#),
+            Expr::Lit(Lit::String("Test String".into()))
+        )
+    }
+
+    #[test]
+    fn bool_false() {
+        assert_eq!(parse_exp("false"), Expr::Lit(Lit::Bool(false)))
+    }
+
+    #[test]
+    fn bool_true() {
+        assert_eq!(parse_exp("true"), Expr::Lit(Lit::Bool(true)))
+    }
+
+    #[test]
+    fn simplest_function_call() {
+        assert_eq!(
+            parse_exp("f()"),
+            Expr::FnCall(b(FnCall {
+                callee: Expr::Id("f".into()),
+                params: vec![]
+            }))
+        )
+    }
+
+    #[test]
+    fn single_param_function_call() {
+        assert_eq!(
+            parse_exp("f(1)"),
+            Expr::FnCall(b(FnCall {
+                callee: Expr::Id("f".into()),
+                params: vec![
+                    Expr::Lit(Lit::Int(1))
+                ]
+            }))
+        )
+    }
+
+    #[test]
+    fn single_param_function_call2() {
+        assert_eq!(
+            parse_exp("f(\"a\")"),
+            Expr::FnCall(b(FnCall {
+                callee: Expr::Id("f".into()),
+                params: vec![
+                    Expr::Lit(Lit::String("a".into()))
+                ]
+            }))
+        )
+    }
+}
